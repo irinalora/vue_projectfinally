67fc60f5385aff5b848f3fd273487af144fe4b1d
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-6.local>
2022-04-07 09:51:25 +0200

    wp

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,6 +1,13 @@
 <template>
-  calendar
-  <div class="grid grid-cols-7">
+  <div class="gap-10 text-center">Mayo, 2022</div>
+  <div class="grid grid-cols-7 gap-10">
+    <div>Mon</div>
+    <div>Tue</div>
+    <div>Wed</div>
+    <div>Thu</div>
+    <div>Fri</div>
+    <div>Sat</div>
+    <div>Sun</div>
     <div v-for="day in days" :key="day">
       {{ day }}
     </div>
